5c3bc705c9a0c062f2422a5822b04e3ca9916ad6
J. Alexis TH <colores.2808.8000@gmail.com>
2023-06-21 10:35:33 -0600

    GET para obtener la lista de categorias(Genres)

--- js/main.js
+++ js/main.js
@@ -21,4 +21,33 @@ async function getPopularMoviesPreview() {
     })
 }
 
+async function getCategoriesPreview() {
+    const response = await fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=' + API_KEY);
+
+    const data = await response.json();
+
+    const categories = data.genres;
+
+
+
+    categories.forEach(category => {
+
+        const categoriesPreviewContainer = document.querySelector('#categoriesPreview .categoriesPreview-list');
+
+        const categoryContainer = document.createElement('div');
+        categoryContainer.classList.add('category-container');
+
+        const categoryTitle = document.createElement('h3');
+        categoryTitle.classList.add('category-title');
+        categoryTitle.setAttribute('id', 'id' + category.id);
+        const categoryTitleText = document.createTextNode(category.name);
+
+        categoryTitle.appendChild(categoryTitleText);
+        categoryContainer.appendChild(categoryTitle);
+        categoriesPreviewContainer.appendChild(categoryContainer);
+
+    })
+}
+
 getPopularMoviesPreview();
+getCategoriesPreview();
