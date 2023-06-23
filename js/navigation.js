9f4f208c62734cbee42c7d9f926374f32521cdcc
J. Alexis TH <colores.2808.8000@gmail.com>
2023-06-23 17:58:48 -0600

    Detalles de una pelicula

--- js/navigation.js
+++ js/navigation.js
@@ -105,6 +105,11 @@ function movieDetailsPage() {
     categoriesPreviewSection.classList.add('inactive');
     genericSection.classList.add('inactive');
     movieDetailSection.classList.remove('inactive');
+
+
+    const [_, movieId] = location.hash.split('=');
+
+    getMovieById(movieId);
 }
 
 function searchPage() {
